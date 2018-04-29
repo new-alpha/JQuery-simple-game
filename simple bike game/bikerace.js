449aab79156fc28c80982ec2ee6195076882928d
Sachin Vashisth <38041862+new-alpha@users.noreply.github.com>
2018-04-30 01:08:11 +0530

    Update bikerace.js

--- simple bike game/bikerace.js
+++ simple bike game/bikerace.js
@@ -1,5 +1,10 @@
+
+//To start the race
+
 $('#gogo').click(function(){
 
+  // function to check whether a car has reached the finish line or not
+  
   function CheckIfComplete(){
     if(isComplete == false){
       isComplete = true;
@@ -8,33 +13,46 @@ $('#gogo').click(function(){
       place = 'second';
     }
   }
-
+// The bikes will stop at the finish line with their tips touching it
+  
 var bikewidth = $('#bike1').width();
 var racetrackwidth=$(window).width() - bikewidth;
 
+  //to generate random timings for bike 
+  
 var bike1time = Math.floor((Math.random()*5000)+1);
 var bike2time = Math.floor((Math.random()*5000)+1);
 
+  // flag
 var isComplete = false;
-
+ // flag
 var place = 'first';
 
+  // animation of first bike
+  
 $('#bike1').animate({
   left:racetrackwidth
 } , bike1time, function(){
     CheckIfComplete();
 
+//To display the timing of first bike
+  
     $('#bike1info span').text('Finished in ' + " "+ place + " " + 'in time' +" "+ bike1time +" "+'milliseconds');
 });
+  //animation of second bike
+  
 $('#bike2').animate({
   left:racetrackwidth
 } , bike2time, function(){
     CheckIfComplete();
-
+//To display the timing of second bike
+  
     $('#bike2info span').text('Finished in ' + " "+ place + " " + 'in time' +" "+ bike2time +" "+'milliseconds');
 });
 
 });
+
+// To reset the race
 
 $('#rereset').click(function(){
 
